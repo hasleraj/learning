60b35f768688ae88b1193e0cf983c6a4671caab6
Ashley Hasler <ashleyhasler9@gmail.com>
2021-03-29 19:10:30 -0300

    Fix formatting

--- react-recoil-todo/src/recoil/todoList/withActive.js
+++ react-recoil-todo/src/recoil/todoList/withActive.js
@@ -3,11 +3,11 @@ import SELECTORS from "../../enums/selectorKeys";
 import todoListAtom from "./todoList";
 
 const withActive = selector({
-    key: SELECTORS.ACTIVETODOS,
-    get: ({get}) => {
+		key: SELECTORS.ACTIVETODOS,
+		get: ({get}) => {
 			const todos = get(todoListAtom);
 			return todos.filter(todo => !todo.completed)
-    },
+		},
 })
 
 export default withActive;
--- react-recoil-todo/src/recoil/todoList/withCompleted.js
+++ react-recoil-todo/src/recoil/todoList/withCompleted.js
@@ -3,11 +3,11 @@ import SELECTORS from "../../enums/selectorKeys";
 import todoListAtom from "./todoList";
 
 const withCompleted = selector({
-    key: SELECTORS.COMPLETEDTODOS,
-    get: ({get}) => {
-        let todos = get(todoListAtom);
-        return todos.filter(todo => todo.completed === true)
-    },
+		key: SELECTORS.COMPLETEDTODOS,
+		get: ({get}) => {
+			let todos = get(todoListAtom);
+			return todos.filter(todo => todo.completed === true)
+		},
 })
 
 export default withCompleted;
